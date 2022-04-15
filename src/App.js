4bf92ca8900e66ed7f7b89d65715024d3535edee
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-15 19:50:13 +0530

    setup mui ♐

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-        Hello world
+      Hello world
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
     </div>
   );
 }
