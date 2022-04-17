6eb5d859a12ae4cf99b5bb198a2e24338196beb3
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-17 21:17:31 +0530

    Added Sora Font using react helmet ⛑

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Builder from "./pages/Builder";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
       <Router>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>ProCVBuilder🎓</title>
+           <link rel="icon" href="https://i.imgur.com/QQQQQQQ.png" />
+           <link rel="preconnect" href="https://fonts.googleapis.com" /> 
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Sora:wght@200;600&display=swap" rel="stylesheet"/>
+      </Helmet>
         <Navbar />
-        <main className="min-h-[70vh]">
+        <main className="min-h-[70vh] font-sora">
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
