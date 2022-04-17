814cdfc049432d094d01c18a0c9ebe6a06d8196c
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-17 20:33:42 +0530

    Updates 🅰

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,10 @@ function Hero() {
               <h1 className="text-5xl font-bold mt-0 mb-6 max-w-[30ch]">
                 Land Your Next Big Offer. Just like all our users did!! 🎓👩‍🎓👨‍🎓
               </h1>
-              <h3 className="text-3xl font-bold mb-8">Subeading</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore magnam porro provident! Omnis molestiae animi voluptates, magnam nisi quod!</p>
               <button
                 type="button"
-                className="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                className="inline-flex px-6 mt-5 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
               >
