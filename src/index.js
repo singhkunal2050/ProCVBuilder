fcfe9872437cfa99471d131fd02b55ca450e7e67
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-18 16:37:08 +0530

    commented react strict mode😭

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { ThemeProvider } from "./context/themeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
