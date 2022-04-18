4212c0dcad37954406479619d7077965d0dd0359
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-18 14:32:13 +0530

    created basic resume template design one ⚡⚡⚡

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Container from "./Container";
- 
+import { Link } from "react-router-dom";
+
 function Hero() {
   return (
     <header>
-      <div
-        className="flex flex-col justify-center items-center py-20 md:py-40 bg-[url('//source.unsplash.com/600x700?job')] bg-cover bg-emerald-300 bg-blend-multiply ">
+      <div className="flex flex-col justify-center items-center py-20 md:py-40 bg-[url('//source.unsplash.com/600x700?job')] bg-cover bg-emerald-300 bg-blend-multiply ">
         <Container>
           <div className="flex justify-center items-center h-full">
             <div className="text-center text-white px-6 md:px-12">
               <h1 className="text-5xl font-bold mt-0 mb-6 max-w-[30ch]">
                 Land Your Next Big Offer. Just like all our users did!! 🎓👩‍🎓👨‍🎓
               </h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore magnam porro provident! Omnis molestiae animi voluptates, magnam nisi quod!</p>
-              <button
-                type="button"
-                className="inline-flex px-6 mt-5 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-              >
-                Get started
-              </button>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus tempore magnam porro provident! Omnis molestiae animi
+                voluptates, magnam nisi quod!
+              </p>
+              <Link to="/builder">
+                <button
+                  type="button"
+                  className="inline-flex px-6 mt-5 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  Get started
+                </button>
+              </Link>
             </div>
           </div>
         </Container>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,14 +3,14 @@ import { FaFacebook, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 function Resume() {
   return (
-    <section className="border-2 border-red-300 flex-1 p-2">
+    <section className="border-2 border-emerald-300 flex-1 p-2">
       <section className="top-section flex">
-        <div className="flex flex-col border-2 border-red-200 p-2 ">
-          <h2 className="text-4xl font-bold">Rahul Singh</h2>
+        <div className="flex flex-col border-2 border-emerald-100 p-2 ">
+          <h2 className="text-4xl font-bold text-emerald-600 ">Rahul Singh</h2>
           <p>Software Engineer</p>
           <p>Pune</p>
         </div>
-        <div className="flex-1 border-2 border-red-200 flex flex-col items-end p-2 ">
+        <div className="flex-1 border-2 border-emerald-100 flex flex-col items-end p-2 ">
           <p>rahul@gmail.com</p>
           <p>8888888888</p>
           <p className="flex flex-col">
@@ -23,45 +23,57 @@ function Resume() {
           </p>
         </div>
       </section>
-      <section className="middle-section border-2 border-red-200 grid grid-cols-60/40 ">
+      <section className="middle-section border-2 border-emerald-100 grid grid-cols-60/40 ">
         <div className="left">
           <div className="experiences">
-            <h2 className="text-2xl font-bold capitalize mb-1 p-2">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">
               Experiences
             </h2>
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">Inmax Infotech</h3>
               <p>Software Engineer</p>
               <p>Jan-20 - Dec-22</p>
+              <ul className="points list-disc ml-6 text-sm">
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+              </ul>
             </article>
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">Inmax Infotech</h3>
               <p>Software Engineer</p>
               <p>Jan-20 - Dec-22</p>
+              <ul className="points list-disc ml-6 text-sm">
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+              </ul>
             </article>
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">Inmax Infotech</h3>
               <p>Software Engineer</p>
               <p>Jan-20 - Dec-22</p>
+              <ul className="points list-disc ml-6 text-sm">
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, omnis?</li>
+              </ul>
             </article>
           </div>
           <div className="education">
-            <h2 className="text-2xl font-bold capitalize mb-1 p-2">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">
               Education
             </h2>
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">BTech</h3>
               <p>Pune University</p>
               <p>2018 -2022</p>
             </article>
            
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">BTech</h3>
               <p>Pune University</p>
               <p>2018 -2022</p>
             </article>
            
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">BTech</h3>
               <p>Pune University</p>
               <p>2018 -2022</p>
@@ -71,37 +83,73 @@ function Resume() {
         </div>
         <div className="right ">
           <div className="skills">
-            <h2 className="text-2xl font-bold capitalize mb-1 p-2">Skills</h2>
-            <article className="border-2 border-red-200 flex gap-2 flex-wrap p-2">
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">Skills</h2>
+            <article className="border-2 border-emerald-100 flex gap-2 flex-wrap p-2">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 HTML
               </span>
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 CSS
               </span>
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 Javascript
               </span>
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 React
               </span>
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 Java
               </span>
-              <span className="py-[2px] px-1 text-sm rounded-lg bg-red-200">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
                 Python
               </span>
             </article>
           </div>
           <div className="certifications">
-            <h2 className="text-2xl font-bold capitalize mb-1 p-2">Certifications</h2>
-             <article className=" border-2 border-red-200 p-2">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">Certifications</h2>
+             <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">Python Bootcamp</h3>
               <p>freecodecamp</p>
             </article>
-            <article className=" border-2 border-red-200 p-2">
+            <article className=" border-2 border-emerald-100 p-2">
               <h3 className="text-lg font-bold">Python Bootcamp</h3>
               <p>freecodecamp</p>
+            </article>
+          </div>
+          <div className="Projects">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">Certifications</h2>
+             <article className=" border-2 border-emerald-100 p-2">
+              <h3 className="text-lg font-bold">Passowrd Generator</h3>
+              <p>Python,Numpy</p>
+              <ul className="points list-disc ml-6 text-sm">
+                  <li>Lorem ipsum ctetur adipisicing elit. Necessitatibus, omnis?</li>
+                  <li>Lorem ipsum elit. Necessitatibus, omnis?</li>
+              </ul>
+            </article>
+            <article className=" border-2 border-emerald-100 p-2">
+              <h3 className="text-lg font-bold">Ecommerce App</h3>
+              <p>React,Nodejs</p>
+              <ul className="points list-disc ml-6 text-sm">
+                  <li>Lorem ipsum ctetur adipisicing elit. Necessitatibus, omnis?</li>
+                  <li>Lorem ipsum elit. Necessitatibus, omnis?</li>
+              </ul>
+            </article>
+          </div>
+          <div className="skills">
+            <h2 className="text-2xl font-bold capitalize mb-1 p-2 text-emerald-600">Languages</h2>
+            <article className="border-2 border-emerald-100 flex gap-2 flex-wrap p-2">
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
+                English
+              </span>
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
+                Hindi
+              </span>
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
+                Marathi
+              </span>
+              <span className="py-[2px] px-1 text-sm rounded-lg bg-emerald-100">
+                Gujarati
+              </span>
             </article>
           </div>
         </div>
