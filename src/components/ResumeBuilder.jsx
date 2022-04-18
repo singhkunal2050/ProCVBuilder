d7a0d5e31ad534876e70561a3c0636162784b153
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-18 16:26:32 +0530

    Added Simple Toggle Button🌠

--- src/components/ResumeBuilder.jsx
+++ src/components/ResumeBuilder.jsx
@@ -1,11 +1,12 @@
-// import Resume from "./Resume";
+import Resume from "./Resume";
 import Resume2 from "./Resume2";
 import ResumeEditor from "./ResumeEditor";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ReactToPrint from "react-to-print";
 
 function ResumeBuilder() {
   const resumeRef = useRef();
+  const [template, settemplate] = useState(false);
 
   return (
     <section className="py-8">
@@ -15,15 +16,15 @@ function ResumeBuilder() {
 
       <ReactToPrint
         trigger={() => (
-          <button className="bg-fuchsia-300 p-2 mx-auto">Print</button>
+          <button className="bg-fuchsia-300 text-black font-bold p-2 mx-auto">Print</button>
         )}
         content={() => resumeRef.current}
       />
 
+      <button className="ml-6 bg-fuchsia-300 text-black font-bold p-2 mx-auto" onClick={() => settemplate(!template)}>Toggle Template</button>
       <section className="flex items-stretch min-h-[1200px]">
-        <div className="flex-1"  ref={resumeRef}>
-          {/* <Resume /> */}
-          <Resume2 />
+        <div className="flex-1" ref={resumeRef}>
+          {template ? <Resume /> : <Resume2 />}
         </div>
         <ResumeEditor />
       </section>
