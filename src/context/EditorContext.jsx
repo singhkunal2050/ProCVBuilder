c3d487f3b2c9f3789bdcda580631e2ecb7b1251a
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-19 18:13:17 +0530

    updated Schema of editor in context🐱‍💻🐱‍💻

--- src/context/EditorContext.jsx
+++ src/context/EditorContext.jsx
@@ -1,38 +1,63 @@
 import { createContext, useContext, useState } from "react";
 
-export const EditorCotext = createContext()
+export const EditorCotext = createContext();
 
 export const useEditor = () => {
-    return useContext(EditorCotext)
-}
+  return useContext(EditorCotext);
+};
 
 export function EditorProvider(props) {
+  const [layout, setLayout] = useState("L1");
 
-    const [layout, setLayout] = useState("L1")
+  const [editor, setEditor] = useState({
+    personal: {
+      firstname: "Mike",
+      lastname: "Alpha",
+      email: "mikealpha@gmail.com",
+      phone: "9999999999",
+      website: "demo.com",
+      facebookLink: "facebook.com",
+      twitterLink: "twitter.com",
+      linkedinLink: "linkedin.com",
+      githubLink: "github.com",
+    },
+    skills: ["HTML", "CSS", "Javascript", "nodejs", "Mongodb"],
+    experiences: [
+      {
+        companyName: "AA Infotech",
+        role: "Software Engineer",
+        from: "2018-Jan",
+        to: "2020-Jan",
+        highlights: [
+          "Developed a web application for managing the inventory of a company",
+          "Developed a web application for managing the inventory of a company",
+          "Developed a web application for managing the inventory of a company",
+        ],
+      },
+      {
+        companyName: "AA Infotech",
+        role: "Software Engineer",
+        from: "2018-Jan",
+        to: "2020-Jan",
+        highlights: [
+          "Developed a web application for managing the inventory of a company",
+          "Developed a web application for managing the inventory of a company",
+          "Developed a web application for managing the inventory of a company",
+        ],
+      },
+    ],
+  });
 
-    const [editor, setEditor] = useState({
-        personal: {
-            firstname: "Mike",
-            lastname: "Alpha",
-            email: "mikealpha@gmail.com",
-            phone: "9999999999",
-            website : "demo.com",
-            facebookLink : "facebook.com",
-            twitterLink : "twitter.com",
-            linkedinLink : "linkedin.com",
-            githubLink : "github.com",
-        },
-        skills: ['HTML' , 'CSS' , 'Javascript', 'nodejs' , 'Mongodb' ]
-    })
+  const value = {
+    editor,
+    setEditor,
+    layout,
+    setLayout,
+  };
 
-
-    const value = {
-        editor,
-        setEditor,
-        layout, setLayout
-    }
-
-    return <EditorCotext.Provider value={value}>
-        {props.children}
+  return (
+    <EditorCotext.Provider value={value}>
+      {props.children}
     </EditorCotext.Provider>
+  );
 }
