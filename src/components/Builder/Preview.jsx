0b53f3a3689a66ddbb2d7260152b879f228cc3d9
singhkunal2050 <singhkunal2050@gmail.com>
2022-05-27 12:40:04 +0530

    udpate

--- src/components/Builder/Preview.jsx
+++ src/components/Builder/Preview.jsx
@@ -1,14 +1,13 @@
 import React, { Suspense } from "react";
 import { useLayout } from "../../context/LayoutContext";
 
-export function Preview({ state }) {
+export function   Preview({ state }) {
   const { resumeRef } = state;
   const { layout } = useLayout(); 
   console.log({"layout rendereed":layout})  
   
 
   const CurrentLayout = React.lazy(() => import("../Layouts/" + layout.template));
-
 
   return (
     <section className={`min-h-[1200px] max-w-4xl ml-[60px] lg:mx-auto `} >
